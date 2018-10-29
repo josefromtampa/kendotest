ee29124c2c87c4a51693106a7e2847c911625eae
= <josefromtampa@gmail.com>
2018-10-29 14:08:38 -0400

    'latest'

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { KendoButtonGroup, KendoButtonInstaller, KendoButtonsInstaller } from '@progress/kendo-buttons-vue-wrapper'
+import '@progress/kendo-ui'
+
 
 Vue.config.productionTip = false
+Vue.use(KendoButtonsInstaller)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App , KendoButtonGroup}
 })
